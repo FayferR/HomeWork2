7936b00af12f9a6b4148aae2baab993cea5c5e2f
FayferR <fayfer07@mail.ru>
2022-03-31 22:09:37 +0600

    Изменил название переменной

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,27 +5,26 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-        int generateRandomAge = (int) (Math.random() * 70);
 
 
         System.out.println(people(15, -20));
         System.out.println(people(38, 25));
-        System.out.println(people(40, 29));
-        System.out.println(people(55, 16));
+        System.out.println(people(40, -30));
+        System.out.println(people(55, 6));
         System.out.println(people(10, 24));
 
     }
 
 
     public static String people(int age, int temp) {
-        String agree = "Можно идти гулять";
+        String goAWalk = "Можно идти гулять";
         String sitAtHome = "Оставайтесь дома";
         if (age > 20 && age < 45 && temp > -20 && temp < 30) {
-            return agree;
+            return goAWalk;
         } else if (age < 20 && temp > 0 && temp < 28) {
-            return agree;
+            return goAWalk;
         } else if (age > 45 && temp > -10 && temp < 25) {
-            return agree;
+            return goAWalk;
         } else {
             return sitAtHome;
         }
